0268644f935df7077403d5721c021d0fbfdfa15e
Rdarke <rickydarke1@gmail.com>
2021-10-22 18:08:59 -0700

    install axios

---
+++ close-buy/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+
+
+  
+  return (
+    <div className="App">
+      <header className="App-header">CloseBuy</header>
+    </div>
+  );
+}
+
+export default App;
