db8e216991e5973d5601c946b24da9d5cddb72a6
Rdarke <rickydarke1@gmail.com>
2021-10-22 18:13:30 -0700

    Update App.jsx with data response from server.

--- close-buy/src/App.jsx
+++ close-buy/src/App.jsx
@@ -4,11 +4,21 @@ import './App.css';
 
 function App() {
 
+  fetchData = () => {
+    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
+    .then((response) => {
+      // handle success
+      console.log("Full response data --- ", response.data) // The entire response from the API
+      console.log("Response data message --- ", response.data.message) // Just the message
+    }) 
+  }
 
-  
   return (
     <div className="App">
       <header className="App-header">CloseBuy</header>
+      <button onClick={this.fetchData} >
+          Fetch Data
+        </button>        
     </div>
   );
 }
