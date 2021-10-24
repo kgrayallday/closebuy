244bb2040acf49afa4af917d8cfe725e152fc525
Kyle Gray <kylemaxgray@gmail.com>
2021-10-23 17:55:31 -0700

    add splash screen component and style

--- close-buy/src/App.jsx
+++ close-buy/src/App.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import Splash from './Splash'
+
+const showSplash = true
 
 function App() {
 
-  const fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log("Full response data --- ", response.data) // The entire response from the API
-      console.log("Response data message --- ", response.data.message) // Just the message
-    }) 
-  }
+  // const fetchData = () => {
+  //   axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
+  //   .then((response) => {
+  //     // handle success
+  //     console.log("Full response data --- ", response.data) // The entire response from the API
+  //     console.log("Response data message --- ", response.data.message) // Just the message
+  //   }) 
+  // }
 
   return (
     <div>
-      <header>CloseBuy</header>
-      <button onClick={fetchData()} >
-          Fetch Data
-        </button>        
+      <Splash />
     </div>
   );
 }
