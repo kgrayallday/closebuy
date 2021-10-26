5030b9d7d0e63c12784e4fc6757232108b263ced
Kyle Gray <kylemaxgray@gmail.com>
2021-10-25 22:38:03 -0700

    clean up code indentation and fic class -> className for JSX files

--- close-buy/src/Navbar.js
+++ close-buy/src/Navbar.js
@@ -2,22 +2,21 @@ import React from 'react';
 import './Navbar.css';
 
 function Navbar() {
-  const something = {/*something*/}
 
   return(
     <nav>
-  <div class="left">
-  <i class="fas fa-couch" id='brand-mark'></i>
-  </div>
+      <div className="left">
+        <i className="fas fa-couch" id='brand-mark'></i>
+      </div>
 
-  <div class="center">
-    CloseBuy
-  </div>
+      <div className="center">
+        <b>Close</b>Buy
+      </div>
 
-  <div class="right">
-    About
-  </div>
-</nav>
+      <div className="right">
+        About
+      </div>
+    </nav>
   )
 
 }
