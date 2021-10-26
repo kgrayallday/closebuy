623993775479a1058392bdfc1e91848d9c50a154
Kyle Gray <kylemaxgray@gmail.com>
2021-10-25 22:18:18 -0700

    cleanup code indentation etc

--- close-buy/src/Search.jsx
+++ close-buy/src/Search.jsx
@@ -9,21 +9,22 @@ function Search(props) {
   // Function passes the search term state, up to App.jsx components
   // Will only provide data onSave event.
   const saveForm = () => {
-    onSave(term)
-    setTerm("")
+    onSave(term);
+    setTerm("");
   }
 
   return (
     <div className="search">
       <form action="/" className="form" onSubmit={event => event.preventDefault()}>
         <input 
-        type="search" 
-        id="search-value" 
-        name="search-value" 
-        placeholder="Surf!" 
-        className="surf-field"
-        value={term}
-        onChange={(event) => setTerm(event.target.value)}/>
+          type="search" 
+          id="search-value" 
+          name="search-value" 
+          placeholder="Surf!" 
+          className="surf-field"
+          value={term}
+          onChange={(event) => setTerm(event.target.value)}
+        />
         <button type="submit" className="surf-button" onClick={saveForm}>
         <i className="fas fa-search"></i>
         </button>
