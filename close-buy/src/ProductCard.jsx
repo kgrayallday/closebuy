bf77eea4309b388df47413c394a07a482e00fa6b
Kyle Gray <kylemaxgray@gmail.com>
2021-10-26 17:55:46 -0700

    better classify className elements

--- close-buy/src/ProductCard.jsx
+++ close-buy/src/ProductCard.jsx
@@ -5,17 +5,17 @@ function ProductCard() {
 
   return(
     <div className="product_container">
-      <div className="container__info">
-        <span><i class="fa-solid fa-dollar-sign"></i>200</span>
+      <div className="product_container__info">
+        <span><i className="fa-solid fa-dollar-sign"></i>200</span>
         {/* <span><i class="fa-solid fa-dollar-sign"></i>200</span> */}
         {/* <span><i class="fa-solid fa-dollar-sign"></i>200</span> */}
       </div>
-      <div className="container__profile">
+      <div className="product_container__profile">
         <img
         src='https://www.kindpng.com/picc/m/559-5595091_craigslist-centralized-network-of-online-communities-logo-hd.png'
         alt="product"
         />
-        <div className="container__profile__text">
+        <div className="product_container__profile__text">
           <h2>Couch Probably</h2>
           <p>This is where you would have a description about your pullout couch</p>
         </div>
@@ -26,3 +26,4 @@ function ProductCard() {
 }
 
 export default ProductCard;
+
