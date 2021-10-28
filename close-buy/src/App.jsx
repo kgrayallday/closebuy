fc2b289f79cb41fafb748bcf1723c3df56aa31fd
Rdarke <rickydarke1@gmail.com>
2021-10-28 14:21:55 -0700

    Npm install react-router-dom

--- close-buy/src/App.jsx
+++ close-buy/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import axios from 'axios';
 import './App.css';
 import Splash from './Splash';
