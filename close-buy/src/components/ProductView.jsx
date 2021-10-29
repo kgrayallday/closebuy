3fdd1a9dfe2f385fb8fe34f8fa9aa62a51f16ea8
Kyle Gray <kylemaxgray@gmail.com>
2021-10-29 11:00:27 -0700

    add styling to ProductFocus Component - WIP

--- close-buy/src/components/ProductView.jsx
+++ close-buy/src/components/ProductView.jsx
@@ -1,5 +1,9 @@
 import React, { useEffect } from 'react';
 import { useParams, useHistory, useLocation } from 'react-router-dom';
+import './ProductView.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 function ProductView() {
   const params = useParams(); // provides url params id.
@@ -9,17 +13,38 @@ function ProductView() {
   console.log("Product params", params) // debuggin to check url params
   console.log("Current state", location.props) // debuggin to check location.props
 
+  const settings = {
+    className: 'product-slider',
+    infinite: false,
+    arrows: true,
+    centerMode: true,
+    centerPadding: '50px', /* 50px is default */
+    draggable: true, /* true is default */
+    lazyLoad: 'ondemand', /* ondemand or progressive - may be useful for many results */
+    /* responsive: - takes an array of breakpoints and settings */
+    slidesToShow: 1, /* currently not working, likely due to css */
+    autoplay: false,
+  }
+
   return(
     <div>
-    <h2>Product View #{params.id}</h2>
-    <span></span>
-    <li>{location.props.id}</li>
-    <li>{location.props.title}</li>
-    <li>{location.props.domain}</li>
-    <li>{location.props.url}</li>
-    <li>{location.props.description}</li>
-    <li>{location.props.category}</li>
-    <li>{location.props.images}</li>
+      <h2>Product View #{params.id}</h2>
+      <div className='product-container'>
+        <Slider {...settings}>
+          <div className='product-img-container'>
+            <img className='product-image' alt='product' src={location.props.images[0]}></img>
+          </div>
+        </Slider>
+        <div className='product-description'>
+          <li>{location.props.id}</li>
+          <li>{location.props.title}</li>
+          <li>{location.props.domain}</li>
+          <li>{location.props.url}</li>
+          <li>{location.props.description}</li>
+          <li>{location.props.category}</li>
+        </div>
+        
+      </div>
     </div>
     
   );
