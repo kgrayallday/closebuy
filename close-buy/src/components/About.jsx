e2ec07be5988d87694ff2b7f9a5e9044870a5a1a
Kyle Gray <kylemaxgray@gmail.com>
2021-10-30 14:49:59 -0700

    style about devs to vertical design side by side

--- close-buy/src/components/About.jsx
+++ close-buy/src/components/About.jsx
@@ -4,49 +4,49 @@ import './About.css';
 function About() {
 
   return(
-    <div>
-      <h2>About The Developers</h2>
+    <div className='about-container'>
+      <h1>About The Developers</h1>
 
-      <div className='about-dev-card'>
-        <h2>Kyle Gray</h2>
-        <div className='about-dev-content'>
-        <div className='about-dev-mug'>
-          <img src='https://res.cloudinary.com/tadabase/image/upload/v1577943236/client/suiteupstairs/placeholder_male1_hn5ybs.jpg'></img>
+      <div className='about-cards-container'>
+        <div className='about-dev-card'>
+          <h2>Kyle Gray</h2>
+          <div className='about-dev-content'>
+          <div className='about-dev-mug'>
+            <img src='https://res.cloudinary.com/tadabase/image/upload/v1577943236/client/suiteupstairs/placeholder_male1_hn5ybs.jpg'></img>
+          </div>
+          <div className='about-dev-desc'>
+            <p> So-and-so is a developer from a place where developers come from. They Developed there and then developments were made that led them here. Now they continue to develope here and there and everywhere. </p>
+          </div>
+          </div>
         </div>
 
-        <div className='about-dev-desc'>
-          <p> So-and-so is a developer from a place where developers come from. They Developed there and then developments were made that led them here. Now they continue to develope here and there and everywhere. </p>
-        </div>
+        <div className='about-dev-card'>
+          <h2>Ricky Darke</h2>
+          <div className='about-dev-content'>
+          <div className='about-dev-mug'>
+            <img src='https://res.cloudinary.com/tadabase/image/upload/v1577943236/client/suiteupstairs/placeholder_male1_hn5ybs.jpg'></img>
+          </div>
+
+          <div className='about-dev-desc'>
+            <p> So-and-so is a developer from a place where developers come from. The Developed there and then developments were made that led them here. Now they continue to develope here and there and everywhere. </p>
+          </div>
+          </div>
+
         </div>
 
-      </div>
+        <div className='about-dev-card'>
+          <h2>Sanghwan Jeon</h2>
+          <div className='about-dev-content'>
+          <div className='about-dev-mug'>
+            <img src='https://res.cloudinary.com/tadabase/image/upload/v1577943236/client/suiteupstairs/placeholder_male1_hn5ybs.jpg'></img>
+          </div>
 
-      <div className='about-dev-card'>
-        <h2>Ricky Darke</h2>
-        <div className='about-dev-content'>
-        <div className='about-dev-mug'>
-          <img src='https://res.cloudinary.com/tadabase/image/upload/v1577943236/client/suiteupstairs/placeholder_male1_hn5ybs.jpg'></img>
-        </div>
+          <div className='about-dev-desc'>
+            <p> So-and-so is a developer from a place where developers come from. The Developed there and then developments were made that led them here. Now they continue to develope here and there and everywhere. </p>
+          </div>
+          </div>
 
-        <div className='about-dev-desc'>
-          <p> So-and-so is a developer from a place where developers come from. The Developed there and then developments were made that led them here. Now they continue to develope here and there and everywhere. </p>
         </div>
-        </div>
-
-      </div>
-
-      <div className='about-dev-card'>
-        <h2>Sanghwan Jeon</h2>
-        <div className='about-dev-content'>
-        <div className='about-dev-mug'>
-          <img src='https://res.cloudinary.com/tadabase/image/upload/v1577943236/client/suiteupstairs/placeholder_male1_hn5ybs.jpg'></img>
-        </div>
-
-        <div className='about-dev-desc'>
-          <p> So-and-so is a developer from a place where developers come from. The Developed there and then developments were made that led them here. Now they continue to develope here and there and everywhere. </p>
-        </div>
-        </div>
-
       </div>
     </div>
   );
