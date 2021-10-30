2bba393cb206d345b9975d4a978b87ee3a603112
Rdarke <rickydarke1@gmail.com>
2021-10-29 17:49:14 -0700

    Add basic css to Category component

--- close-buy/src/components/Category.jsx
+++ close-buy/src/components/Category.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import './Category.css';
 
 function Category(props) {
-  const { listings } = props
+  const { listings } = props;
+  const history = useHistory();
 
   return (
     <div className='category-parent'>
-        {listings()}     
+        <button className="return-button" onClick={() => history.goBack()}
+        >Return To Search</button>
+      <div className="category-child">
+        {listings()}
+      </div>
     </div>
   );
 }
