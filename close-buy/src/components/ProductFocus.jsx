58926a3a17fcbc962960e5057deed152e34b5283
Kyle Gray <kylemaxgray@gmail.com>
2021-10-30 18:48:34 -0700

    style slide components of carousel

--- close-buy/src/components/ProductFocus.jsx
+++ close-buy/src/components/ProductFocus.jsx
@@ -14,7 +14,7 @@ function ProductView() {
   console.log("Current state", location.props) // debuggin to check location.props
 
   const productSettings = {
-    className: 'product-slider',
+    className: 'pf-slider',
     infinite: false,
     arrows: true,
     dots: true,
@@ -27,7 +27,7 @@ function ProductView() {
   }
 
   const imagesArray = location.props.images.map((url, index) => {
-    return <img key={index} src={url} className='product-image' alt='product'></img>
+    return <div className='pf-image'><img key={index} src={url} className='pf-image' alt='product'></img></div>
   })
 
   return(
