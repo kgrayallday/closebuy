5af028d30779177b41d53aad54d5fbb4c7eed68e
Rdarke <rickydarke1@gmail.com>
2021-11-01 18:14:55 -0700

    Update Favorites.jsx to include css styling similar to categoryviews

--- close-buy/src/components/Favorites.jsx
+++ close-buy/src/components/Favorites.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import './Category.css';
 
 function Favorites(props) {
   const { favoriteListings } = props;
+  const history = useHistory();
 
   return(
-    <div>
+    <div className='category-parent'>
     <h2>Favourties page!</h2>
+    <button className="return-button" onClick={() => history.goBack()}
+    >Return To Search</button>
+    <div className='category-child'>
       {favoriteListings()}
+    </div>
     </div>
   )
 };
