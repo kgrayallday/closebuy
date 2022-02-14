3485129cd96cc7e6712bba964f73b219fc8997c2
Kyle Gray <kylemaxgray@gmail.com>
2022-02-14 14:08:15 -0800

    add dummy href to icon/links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,20 +6,20 @@ function Footer() {
     <footer>
       <div className='footer-content'>
         <div className='footer-inner'>
-          <img src='https://github.com/kgrayallday/closebuy/blob/design/close-buy/public/logo192.png?raw=true'/>
+          <img src='https://github.com/kgrayallday/closebuy/blob/design/close-buy/public/logo192.png?raw=true' alt="logo192"/>
           <div className='footer-copy'>
             <p> CloseBuy was build and designed by </p>
             <p> Kyle Gray, Ricky Darke and Sanghwan Jeon</p>
           </div>
           <div className='footer-socials'>
 
-            <a><i className="fab fa-facebook-f fa-lg"></i></a>
+            <a><i className="fab fa-facebook-f fa-lg" href="www.facebook.com"></i></a>
 
-            <a><i className="fab fa-twitter fa-lg"></i></a>
+            <a><i className="fab fa-twitter fa-lg" href="www.twitter.com"></i></a>
 
-            <a><i className="fab fa-google fa-lg"></i></a>
+            <a><i className="fab fa-google fa-lg" href="www.google.com"></i></a>
 
-            <a><i className="fab fa-instagram fa-lg"></i></a>
+            <a><i className="fab fa-instagram fa-lg" href="www.instagram.com"></i></a>
           </div>
         </div>
       </div>
